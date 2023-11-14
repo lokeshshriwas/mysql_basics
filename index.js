830af140c5c36e6b9ac9f5b29f96ac1955ff67c0
lokesh shriwas <lokeshshriwas2802@gmail.com>
2023-11-14 19:25:39 +0530

    Added schema.sql file and inserting value using js in database

--- index.js
+++ index.js
@@ -1,6 +1,32 @@
 const { faker } = require('@faker-js/faker');
+const mysql = require('mysql2');
 
-let  createRandomUser = () => {
+const connection = mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  database: 'delta_app',
+  password: "Lokesh@2802"
+});
+
+let q = "INSERT INTO user (id, username, email, password) VALUES ?"
+let userdata = [
+  ["123c", "abcc", "abc@gmail.comc", "abc123c"],
+  ["123b", "abcb", "abc@gmail.comm", "abc1234"]
+  ]
+
+try{
+  connection.query(q, [userdata], (err, result)=>{
+    if(err) throw err
+    console.log(result)
+    // console.log(result.length)
+  })
+}catch (err) {
+    console.log(err)
+  }
+
+  connection.end()
+  
+  let  createRandomUser = () => {
   return {
     id: faker.string.uuid(),
     username: faker.internet.userName(),
@@ -9,4 +35,4 @@ let  createRandomUser = () => {
   };
 }
 
-console.log(createRandomUser())
+// console.log(createRandomUser())
