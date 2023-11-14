c86447e380b6153956a814fe432fb2bf4ae3a1c5
lokesh shriwas <lokeshshriwas2802@gmail.com>
2023-11-14 22:53:03 +0530

    backend and dynamic file created

--- index.js
+++ index.js
@@ -1,6 +1,14 @@
 const { faker } = require('@faker-js/faker');
 const mysql = require('mysql2');
 
+const express = require('express')
+const app = express()
+const path = require("path")
+
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "./views"))
+
+
 const connection = mysql.createConnection({
   host: 'localhost',
   user: 'root',
@@ -8,35 +16,51 @@ const connection = mysql.createConnection({
   password: "Lokesh@2802"
 });
 
-let q = "INSERT INTO user (id, username, email, password) VALUES ?"
-
-  let data  = []
-
-  let createRandomUser = () => {
-    return [
-      faker.string.uuid(),
-      faker.internet.userName(),
-      faker.internet.email(),
-      faker.internet.password()
-  ];
-  };
 
 
-  for(let i = 1; i <= 100; i++){
-    data.push(createRandomUser())
-  }
 
-
-try{
-  connection.query(q, [data], (err, result)=>{
-    if(err) throw err
-    console.log(result)
-    // console.log(result.length)
+  app.get('/', function (req, res) {
+    let q = `SELECT count(*) FROM user`
+    try{
+      connection.query(q, (err, result)=>{
+        if(err) throw err
+        let count = result[0]["count(*)"]
+        res.render("home", { count })
+      })
+    }catch (err) {
+       res.send("error")
+    }
   })
-}catch (err) {
-    console.log(err)
-  }
 
-  connection.end()
 
+  app.get('/user', function (req, res) {
+    let q = `SELECT * FROM user`
+    try{
+      connection.query(q, (err, users)=>{
+        if(err) throw err
+        res.render("userpage", { users })
+      })
+    }catch (err) {
+       res.send("error")
+    }
+  })
+
+  
+  app.listen(3000, ()=>{
+    console.log("server is listening to 3000")
+  })
+
+
+
+  
+  // let data  = []
+
+  // let createRandomUser = () => {
+  //   return [
+  //     faker.string.uuid(),
+  //     faker.internet.userName(),
+  //     faker.internet.email(),
+  //     faker.internet.password()
+  // ];
+  // };
 
