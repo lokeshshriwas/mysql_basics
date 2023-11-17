45f8961451eb871a3908906b975db7d9e872be77
lokesh shriwas <lokeshshriwas2802@gmail.com>
2023-11-17 20:31:57 +0530

    edit & delete function added

--- index.js
+++ index.js
@@ -1,13 +1,18 @@
 const { faker } = require('@faker-js/faker');
 const mysql = require('mysql2');
-
 const express = require('express')
 const app = express()
 const path = require("path")
+var methodOverride = require('method-override');
+const { PassThrough } = require('stream');
 
+app.use(methodOverride('_method'))
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "./views"))
+app.use(express.urlencoded({extended: true}))
 
+
+// connecting to database using mysql2 package
 
 const connection = mysql.createConnection({
   host: 'localhost',
@@ -16,8 +21,7 @@ const connection = mysql.createConnection({
   password: "Lokesh@2802"
 });
 
-
-
+  // home route
 
   app.get('/', function (req, res) {
     let q = `SELECT count(*) FROM user`
@@ -32,6 +36,7 @@ const connection = mysql.createConnection({
     }
   })
 
+  // show all users route
 
   app.get('/user', function (req, res) {
     let q = `SELECT * FROM user`
@@ -45,22 +50,87 @@ const connection = mysql.createConnection({
     }
   })
 
+
+  // edit route 
+
+  app.get("/user/:id/edit", (req, res)=>{
+    let {id} = req.params
+    let q = `SELECT * FROM user where id = "${id}"`
+    try {
+    connection.query(q, (err, result)=>{
+      if(err) throw err
+      let editableObj = result[0]
+      res.render("edit", {editableObj})
+    } )
+    } catch (err) {
+      res.send(err)
+    }
+
+  })
+
+  // patch req of edited username
+
+  app.patch("/user/:id", (req, res) => {
+    let {id} = req.params
+    let {username: formUser, password : formPass} =  req.body 
+    let q = `SELECT * FROM user where id = "${id}"`
+    try {
+    connection.query(q, (err, result)=>{
+      if(err) throw err
+      let user = result[0]
+      if(formPass != user.password){
+        res.send("wrong password")
+      }else{
+        let q2 = `UPDATE user SET username ="${formUser}" WHERE id = '${id}'`
+        connection.query(q2, (err, result)=>{
+          if(err) throw err
+          res.redirect('/user')
+        })
+      }
+    } )
+    } catch (err) {
+      res.send(err)
+    }
+    
+  })
+
+  // delete route
+
+  app.get("/user/:id/delete", (req, res)=>{
+    let {id} = req.params;
+    let q = `SELECT * FROM user WHERE id = '${id}'`
+    connection.query(q, (err, result)=>{
+      if(err) throw err
+      let user = result[0]
+      res.render("delete", {user})
+    })
+  })
+
+  app.delete("/user/:id", (req, res)=>{
+    let {id} = req.params
+    let {password: formPass} = req.body 
+   
+    let q = `SELECT * FROM user WHERE id = "${id}"`
+    connection.query(q, (err, result)=>{
+      if(err) throw err
+      let user  = result[0]
+
+      if(formPass != user.password){
+        console.log(user.password)
+        res.send("wrong password")
+      } else{
+        let q2 = `DELETE FROM user WHERE id = "${id}"`
+        connection.query(q2, (err, result)=>{
+          if(err) throw err
+          res.redirect("/user")
+        })
+      }
+    })
+  })
   
   app.listen(3000, ()=>{
     console.log("server is listening to 3000")
   })
 
 
-
-  
-  // let data  = []
-
-  // let createRandomUser = () => {
-  //   return [
-  //     faker.string.uuid(),
-  //     faker.internet.userName(),
-  //     faker.internet.email(),
-  //     faker.internet.password()
-  // ];
-  // };
 
